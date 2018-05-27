1aac12fb826c0396bdee6aa91f9abb3ce2e4b3bb
Giulia Berto <gberto@i4.karst.uits.iu.edu>
2018-05-27 16:24:00 -0400

    update

---
+++ afqConverterMulti.m
@@ -0,0 +1,38 @@
+function out = afqConverterMulti(afq_filename, ref_src_filename)
+
+if ~isdeployed
+	addpath(genpath('/N/u/hayashis/BigRed2/git/vistasoft'));
+	addpath(genpath('/N/u/brlife/git/jsonlab'));
+	addpath(genpath('/N/u/brlife/git/o3d-code'));
+	addpath(genpath('/N/u/brlife/git/encode'));
+end
+
+load(fullfile(char(afq_filename)));
+ref_src = fullfile(char(ref_src_filename));
+
+%convert afq to trk
+disp('Converting afq to .trk');
+
+%sub1
+fid=fopen('tract_name_list.txt', 'w');
+
+if (config.tract1 > 0)
+    for tract = [config.tract1, config.tract2, config.tract3, config.tract4]
+        if (tract > 0)
+            tract_name=strrep(fg_classified(tract).name,' ','_');
+            write_fg_to_trk(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
+            fprintf(fid, [tract_name, '\n']);
+        end    
+    end    
+else
+    for tract=1:length(fg_classified)
+        tract_name=strrep(fg_classified(tract).name,' ','_');
+        write_fg_to_trk(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
+        fprintf(fid, [tract_name, '\n']);  
+    end 
+end
+
+fclose(fid);
+
+exit;
+end
---
+++ lifeConverterMulti.m
@@ -0,0 +1,27 @@
+function out = lifeConverterMulti(fe_filename, ref_src_filename)
+
+if ~isdeployed
+    disp('adding path')
+    addpath(genpath('/N/u/brlife/git/vistasoft'));
+    addpath(genpath('/N/u/brlife/git/jsonlab'));
+    addpath(genpath('/N/u/brlife/git/o3d-code'));
+    addpath(genpath('/N/u/brlife/git/encode'));
+end
+
+% Convert positively-weighted streamlines from LiFE into .trk
+if ischar(fe_filename)
+	fe_src = fullfile(fe_filename);
+else
+	fe_src = fullfile(char(fe_filename));
+end 
+
+if ischar(ref_src_filename)
+	ref_src = fullfile(ref_src_filename);
+else
+	ref_src_src = fullfile(char(ref_src_filename));
+end
+
+fe2trk(fe_src, ref_src, 'output.trk');
+
+exit;
+end
