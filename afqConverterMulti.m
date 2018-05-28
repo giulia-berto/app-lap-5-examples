62726e859f8d856ae0c50e996cb3f190923845c1
Giulia Berto <gberto@i4.karst.uits.iu.edu>
2018-05-27 21:56:28 -0400

    update

--- afqConverterMulti.m
+++ afqConverterMulti.m
@@ -7,6 +7,7 @@ if ~isdeployed
 	addpath(genpath('/N/u/brlife/git/encode'));
 end
 
+config = loadjson('config.json');
 load(fullfile(char(afq_filename)));
 ref_src = fullfile(char(ref_src_filename));
 
@@ -16,21 +17,25 @@ disp('Converting afq to .trk');
 %sub1
 fid=fopen('tract_name_list.txt', 'w');
 
-if (config.tract1 > 0)
-    for tract = [config.tract1, config.tract2, config.tract3, config.tract4]
-        if (tract > 0)
-            tract_name=strrep(fg_classified(tract).name,' ','_');
-            write_fg_to_trk(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
-            fprintf(fid, [tract_name, '\n']);
-        end    
-    end    
-else
-    for tract=1:length(fg_classified)
-        tract_name=strrep(fg_classified(tract).name,' ','_');
-        write_fg_to_trk(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
-        fprintf(fid, [tract_name, '\n']);  
-    end 
-end
+%if (config.tract1 > 0)
+%    for tract = [config.tract1, config.tract2, config.tract3, config.tract4]
+%        if (tract > 0)
+%            tract_name=strrep(fg_classified(tract).name,' ','_');
+%            write_fg_to_trk(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
+%            fprintf(fid, [tract_name, '\n']);
+%        end    
+%    end    
+%else
+%    for tract=1:length(fg_classified)
+%        tract_name=strrep(fg_classified(tract).name,' ','_');
+%        write_fg_to_trk(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
+%        fprintf(fid, [tract_name, '\n']);  
+%    end 
+%end
+
+tract_name=strrep(fg_classified(config.tract1).name,' ','_');
+write_fg_to_trk(fg_classified(config.tract1),ref_src,sprintf('%s_tract.trk',tract_name));
+fprintf(fid, [tract_name, '\n']);
 
 fclose(fid);
 
