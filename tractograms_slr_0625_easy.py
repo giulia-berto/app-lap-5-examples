51f39dc49fbd47139462fdfcb46ead0cdd836da5
Giulia Berto <gberto@hm008.karst.uits.iu.edu>
2018-09-26 08:36:28 -0400

    update

---
+++ tractograms_slr_0625_easy.py
@@ -0,0 +1,93 @@
+""" SLR (Streamline Linear Registration) of two tractograms.
+    
+	See Garyfallidis et. al, 'Robust and efficient linear registration 
+	of white-matter fascicles in the space of streamlines', 
+	Neuroimage, 117:124-140, 2015.
+"""
+
+import os
+import sys
+import argparse
+import os.path
+import nibabel as nib
+import numpy as np
+import pickle
+from os.path import isfile
+import ntpath
+from nibabel.streamlines import load
+from dipy.segment.clustering import QuickBundles
+from dipy.align.streamlinear import StreamlineLinearRegistration
+from dipy.tracking.streamline import set_number_of_points
+from dipy.tracking.utils import length
+
+
+def resample_tractogram(tractogram, step_size):
+    """Resample the tractogram with the given step size.
+    """
+    lengths=list(length(tractogram))
+    tractogram_res = []
+    for i, f in enumerate(tractogram):
+	nb_res_points = np.int(np.floor(lengths[i]/step_size))
+	tmp = set_number_of_points(f, nb_res_points)
+	tractogram_res.append(tmp)
+    return tractogram_res
+
+
+def tractograms_slr(mov_id, moving_tractogram, stat_id, static_tractogram):
+
+	print("Loading tractograms...")
+	moving_tractogram = nib.streamlines.load(moving_tractogram)
+	moving_tractogram = moving_tractogram.streamlines
+	static_tractogram = nib.streamlines.load(static_tractogram)
+	static_tractogram = static_tractogram.streamlines     
+
+	print("Resampling tractograms with step size = 0.625 mm") 
+	static_tractogram_res = resample_tractogram(static_tractogram, step_size=0.625)	
+	static_tractogram = static_tractogram_res
+	moving_tractogram_res = resample_tractogram(moving_tractogram, step_size=0.625)	
+	moving_tractogram = moving_tractogram_res
+
+	print("Set parameters as in Garyfallidis et al. 2015.") 
+	threshold_length = 40.0 # 50mm / 1.25
+	qb_threshold = 16.0  # 20mm / 1.25 
+	nb_res_points = 20
+
+	print("Performing QuickBundles of static tractogram and resampling...")
+	st = np.array([s for s in static_tractogram if len(s) > threshold_length], dtype=np.object)
+	qb = QuickBundles(threshold=qb_threshold)
+	st_clusters = [cluster.centroid for cluster in qb.cluster(st)]
+	st_clusters = set_number_of_points(st_clusters, nb_res_points)
+
+	print("Performing QuickBundles of moving tractogram and resampling...")
+	mt = np.array([s for s in moving_tractogram if len(s) > threshold_length], dtype=np.object)
+	qb = QuickBundles(threshold=qb_threshold)
+	mt_clusters = [cluster.centroid for cluster in qb.cluster(mt)]
+	mt_clusters = set_number_of_points(mt_clusters, nb_res_points)
+
+	print("Performing Linear Registration...")
+	srr = StreamlineLinearRegistration()
+	srm = srr.optimize(static=st_clusters, moving=mt_clusters)
+
+	print("Affine transformation matrix with Streamline Linear Registration:")
+	affine = srm.matrix
+	print('%s' %affine)
+	np.save('m%s_s%s_affine' %(mov_id, stat_id), affine)
+
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-mov_id', nargs='?', const=1, default='',
+	                    help='The moving subject filename')
+	parser.add_argument('-moving', nargs='?', const=1, default='',
+	                    help='The moving tractogram filename')
+	parser.add_argument('-stat_id', nargs='?',  const=1, default='',
+	                    help='The static subject filename')
+	parser.add_argument('-static', nargs='?',  const=1, default='',
+	                    help='The static tractogram filename')                   
+	args = parser.parse_args()
+
+	tractograms_slr(args.mov_id, args.moving, args.stat_id, args.static)	
+	                            
+	sys.exit()    
+
