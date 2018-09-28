d3d00d01d043fc02a9d7b161a1dd7d500ee4e27c
Giulia Berto <gberto@hm008.karst.uits.iu.edu>
2018-09-28 09:07:23 -0400

    fixed bug in compute dsc using right function with shift

--- afqConverter1.m
+++ afqConverter1.m
@@ -1,9 +1,10 @@
 function out = afqConverter1()
 
-addpath(genpath('/N/u/hayashis/BigRed2/git/vistasoft'));
-addpath(genpath('/N/u/brlife/git/jsonlab'));
-addpath(genpath('/N/u/brlife/git/o3d-code'));
-addpath(genpath('/N/u/brlife/git/encode'));
+if ~isdeployed
+	addpath(genpath('/N/u/brlife/git/vistasoft'));
+	addpath(genpath('/N/u/brlife/git/jsonlab'));
+	addpath(genpath('/N/u/brlife/git/o3d-code'));
+end
 
 config = loadjson('config.json');
 ref_src = fullfile(config.t1_static);
@@ -18,16 +19,18 @@ if (config.tract1 > 0)
     for tract = [config.tract1, config.tract2, config.tract3, config.tract4]
         if (tract > 0)
             tract_name=strrep(fg_classified(tract).name,' ','_');
-            write_fg_to_trk(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
+            write_fg_to_trk_shift(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
         end    
     end    
 else
     for tract=1:20
         tract_name=strrep(fg_classified(tract).name,' ','_');
-        write_fg_to_trk(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
+        write_fg_to_trk_shift(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
         fprintf(fid, [tract_name, '\n']);  
     end 
 end
 
 exit;
 end
+
+
